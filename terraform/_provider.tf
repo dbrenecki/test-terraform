b0671ae5b9890a57d8f3e0cd335dd45133892179
dbrenecki <dbrenecki@protonmail.com>
2023-10-23 08:19:31 +1030

    depbot test

---
+++ terraform/_provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+        source = "hashicorp/google"
+        version = "~> 4.84.0"
+    }
+    google-beta = {
+      source = "hashicorp/google-beta"
+      version = "~> 4.84.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.5.1"
+    }
+  }
+  required_version = "~> 1.5.5"
+}
